368f663f7b9b9f2c2301e5ebe1d71d5b62fcaf55
mevx <13dogbite37@gmail.com>
2016-09-11 02:11:15 +0200

    cleanup

---
+++ LBA2HD/Assets/Scripts/MenuSky.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSky : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	float rot;
+	// Update is called once per frame
+	void Update () {
+
+		rot += 0.6f * Time.deltaTime;
+		rot %= 360;
+
+		RenderSettings.skybox.SetFloat("_Rotation",rot);
+
+	}
+}
---
+++ LBA2HD/Assets/Scripts/MovingSkybox.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class MovingSkybox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	float rot;
+	// Update is called once per frame
+	void Update () {
+
+		rot += 0.15f * Time.deltaTime;
+		rot %= 360;
+
+		RenderSettings.skybox.SetFloat("_Rotation",rot);
+
+	}
+}
